24b25ed9a3d55a60ad66bf3701e504f52fd28762
Aishwarya <aishwarya.verma28@gmail.com>
2023-01-18 00:31:52 +0530

    todo with functional components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.css";
+import TodoFunc from "./TodoFunc";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <TodoFunc/>
+  </>
+);
+     
